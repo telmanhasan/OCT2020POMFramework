d9b6b5e289c21caf75a6ad94629d9c65c6c4e190
telmanhasan <70407995+telmanhasan@users.noreply.github.com>
2020-10-08 02:53:46 -0400

    Update AdvProfilePage.java

--- src/main/java/com/qa/hubspot/pages/AdvProfilePage.java
+++ src/main/java/com/qa/hubspot/pages/AdvProfilePage.java
@@ -5,6 +5,8 @@ public class AdvProfilePage {
 	public AdvProfilePage() {
 		System.out.println(" Adv profile page.....");
 		System.out.println(" page class for adv profile page");
+		System.out.println(" karabakh");
+		
 	}
 
 }
